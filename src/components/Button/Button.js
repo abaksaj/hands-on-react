e19fbdab910da5f9ca2c46ad2c6dfb6f8ba09014
Ana Baksaj <ana.baksaj55@gmail.com>
2022-04-30 14:26:47 +0200

    initial commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,25 @@
+import "../../components/Button/Button.scss";
+
+const Button = ({modifiers, children}) => {
+    const modifierClasses = {
+        secondary: 'Button_secondary',
+        nav: 'Button_nav',
+        landing: 'Button_landing',
+        heading: 'Button_heading',
+        outline: 'Button_outline'
+    };
+
+    let buttonClass = "Button";
+
+    modifiers.forEach(modifier => (buttonClass += " " + modifierClasses[modifiers]));
+
+    return (
+
+        <button className={buttonClass}>{children}</button>
+
+    );
+
+
+};
+
+export default Button;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import "../../components/Main/Main.scss";
+
+const Main = (props) => {
+    return (
+
+        <main className="Main">{props.children}</main>
+
+    );
+
+
+};
+
+export default Main;
